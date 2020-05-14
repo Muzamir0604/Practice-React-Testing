be4e82fd1e7ccbf96cd38e164b94404abbbb7e4f
Muhammad Muzamir <mu-za-mir@hotmail.com>
2020-05-14 19:25:17 +0200

    Add proptypes for Congrats component

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,10 +2,13 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 
-import { findByTestAttr } from "../test/testUtils";
+import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = { success: false };
+
 /**
  * Factory function to create a shallowwrapper for the Congrats component
  * @function setup
@@ -13,22 +16,28 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * @return {ShallowWrapper}
  */
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test("renders without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
 test("renders no text when `props` is false", () => {
   const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
-  expect(component.text()).toBe('');
+  expect(component.text()).toBe("");
 });
 
 test("render non-empty congrats message when success prop is true", () => {
-    const wrapper =  setup({success: true});
-    const message = findByTestAttr(wrapper, 'congrats-message');
-    expect(message.text().length).not.toBe(0);
+  const wrapper = setup({ success: true });
+  const message = findByTestAttr(wrapper, "congrats-message");
+  expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warning with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
--- test/testUtils.js
+++ test/testUtils.js
@@ -1,3 +1,5 @@
+import CheckPropTypes from 'check-prop-types';
+
 /**
  * @function findByTestAttr
  * @param {ShallowWrapper} wrapper 
@@ -7,3 +9,13 @@
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
 };
+
+export const checkProps = (component, conformingProps)=>{
+    const propError = CheckPropTypes(
+        component.propTypes,
+        conformingProps,
+        'prop',
+        component.name
+    );
+    expect(propError).toBeUndefined();
+}
