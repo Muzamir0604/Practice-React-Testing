c552ff09029d412390aa3d91efe028bf6d559350
Muhammad Muzamir <mu-za-mir@hotmail.com>
2020-05-15 11:20:35 +0200

    tests and code non-zero guessedWords

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { shallow, ShallowWrapper } from "enzyme";
+import { shallow } from "enzyme";
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import GuessedWords from "./GuessedWords";
 
-const defaultProps = 
-  {
-    guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
-  };
+const defaultProps = {
+  guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }],
+};
 
 /**
  * Factory function create ShallowWrapper for the GuessedWords component
@@ -19,26 +18,45 @@ const setup = (props = {}) => {
   return shallow(<GuessedWords {...setupProps} />);
 };
 
-test('does not throw warning with expected props',()=>{
-    checkProps(GuessedWords, defaultProps);
+test("does not throw warning with expected props", () => {
+  checkProps(GuessedWords, defaultProps);
 });
 
-describe('if there are no words guessed',()=>{
-    let wrapper
-    beforeEach(()=>{
-        wrapper = setup({ guessedWords: []});
-    });
-    test('renders without error',()=>{
-        const component =  findByTestAttr(wrapper,'component-guessed-words');
-        expect(component.length).toBe(1);
-    });
-    test('renders instructions to guess a word',()=>{
-        const instructions = findByTestAttr(wrapper, 'guessed-instructions')
-        expect(instructions.text().length).not.toBe(0)
-    });
-
+describe("if there are no words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  test("renders instructions to guess a word", () => {
+    const instructions = findByTestAttr(wrapper, "guessed-instructions");
+    expect(instructions.text().length).not.toBe(0);
+  });
 });
 
-describe('if there are words guessed',()=>{
-
+describe("if there are words guessed", () => {
+  const guessedWords = [
+    { guessedWords: "train", letterMatchCount: 3 },
+    { guessedWords: "agile", letterMatchCount: 1 },
+    { guessedWords: "party", letterMatchCount: 5 },
+  ];
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  });
+  test("renders without error", () => {
+    const component = findByTestAttr(wrapper, "component-guessed-words");
+    expect(component.length).toBe(1);
+  });
+  test('renders "guessed words" section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-words");
+    expect(guessedWordsNode.length).toBe(1);
+  });
+  test("correct number of guessed words", () => {
+    const guessedWordsNode = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordsNode.length).toBe(guessedWords.length);
+  });
 });
