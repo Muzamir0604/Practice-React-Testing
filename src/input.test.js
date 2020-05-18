8ab0a3bcdcb5c55c5096658f9f224338b6b0d26e
Muhammad Muzamir <mu-za-mir@hotmail.com>
2020-05-18 14:22:05 +0200

    Write tests for Input success=true

--- src/input.test.js
+++ src/input.test.js
@@ -4,6 +4,7 @@ import { shallow, ShallowWrapper } from "enzyme";
 import { findByTestAttr, storeFactory } from "../test/testUtils";
 import Input from "./Input";
 
+//Consider looking into redux-mock-store
 /**
  * Factory functio create a ShallowWrapper for the GuessedWords component.
  * @function setup
@@ -12,23 +13,53 @@ import Input from "./Input";
  */
 const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
-  const wrapper = shallow(<Input store={store} />).dive().dive();
+  const wrapper = shallow(<Input store={store} />)
+    .dive()
+    .dive();
   //using wrapper.debug is handy to see rendered code
-//   console.log(wrapper.debug());
-  return wrapper
+  //   console.log(wrapper.debug());
+  return wrapper;
 };
 setup();
 
 describe("render", () => {
   describe("word has not been guessed", () => {
-    test("renders component without error", () => {});
-    test("renders input box", () => {});
-    test("renders submit buttons", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test("renders component without error", () => {
+        const component =  findByTestAttr(wrapper,"component-input");
+        expect(component.length).toBe(1)
+    });
+    test("renders input box", () => {
+        const inputBox =  findByTestAttr(wrapper,"input-box");
+        expect(inputBox.length).toBe(1)
+    });
+    test("renders submit buttons", () => {
+        const submitButton =  findByTestAttr(wrapper,"submit-button");
+        expect(submitButton.length).toBe(1)
+    });
   });
   describe("word has been guessed", () => {
-    test("does not renders component without error", () => {});
-    test("does not renders input box", () => {});
-    test("does not renders submit buttons", () => {});
+      let wrapper;
+      beforeEach(()=>{
+          const initialState = { success: true};
+          wrapper = setup(initialState)
+      })
+    test("renders component without error", () => {
+        const component = findByTestAttr(wrapper,"component-input");
+        expect(component.length).toBe(1)
+    });
+    test("does not renders input box", () => {
+        const inputBox = findByTestAttr(wrapper, "input-box");
+        expect(inputBox.length).toBe(0);
+    });
+    test("does not renders submit buttons", () => {
+        const submitButton = findByTestAttr(wrapper,"submit-button");
+        expect(submitButton.length).toBe(0)
+    });
   });
 });
 
