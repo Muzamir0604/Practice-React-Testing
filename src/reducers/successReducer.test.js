f64fe33b0e04052dcd39d841e6df044e8753ce3d
Muhammad Muzamir <mu-za-mir@hotmail.com>
2020-05-29 17:56:58 +0200

    added success reducer edit when RESET action added

--- src/reducers/successReducer.test.js
+++ src/reducers/successReducer.test.js
@@ -9,3 +9,8 @@ test('returns state of true upon receiving an action of type `CORRECT_GUESS`',()
     const newState = successReducer(undefined, {type: actionTypes.CORRECT_GUESS})
     expect(newState).toBe(true)
 });
+test('returns state of true upon receiving an action of type `RESET_GAME`',()=>{
+    //start with true
+    const newState=successReducer(true,{type: actionTypes.RESET_GAME});
+    expect(newState).toBe(false)
+});
